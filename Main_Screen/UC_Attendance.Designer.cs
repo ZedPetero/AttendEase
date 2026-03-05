f089aa4c615cce7dc4204eeb7de424779b35b3eb
Pete Zedrick Petero <peterozed@gmail.com>
2026-03-05 21:07:38 +0800

    ye

--- Main_Screen/UC_Attendance.Designer.cs
+++ Main_Screen/UC_Attendance.Designer.cs
@@ -58,15 +58,8 @@
             // lblSectionName
             // 
             lblSectionName.AutoSize = true;
-<<<<<<< HEAD
             lblSectionName.Font = new Font("Inter", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblSectionName.Location = new Point(99, 70);
-=======
-            lblSectionName.BackColor = Color.White;
-            lblSectionName.Font = new Font("Segoe UI", 28F, FontStyle.Bold);
-            lblSectionName.ForeColor = Color.FromArgb(29, 37, 48);
-            lblSectionName.Location = new Point(109, 68);
->>>>>>> features2/balansag/settings
             lblSectionName.Name = "lblSectionName";
             lblSectionName.Size = new Size(197, 45);
             lblSectionName.TabIndex = 1;
@@ -139,62 +132,18 @@
             // lblSubjectName
             // 
             lblSubjectName.AutoSize = true;
-<<<<<<< HEAD
             lblSubjectName.Font = new Font("Inter SemiBold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblSubjectName.ForeColor = Color.FromArgb(108, 124, 137);
             lblSubjectName.Location = new Point(108, 118);
-=======
-            lblSubjectName.Font = new Font("Segoe UI", 16F);
-            lblSubjectName.ForeColor = Color.FromArgb(29, 37, 48);
-            lblSubjectName.Location = new Point(116, 122);
->>>>>>> features2/balansag/settings
             lblSubjectName.Name = "lblSubjectName";
             lblSubjectName.Size = new Size(132, 27);
             lblSubjectName.TabIndex = 4;
             lblSubjectName.Text = "Mathematics";
             // 
-<<<<<<< HEAD
             // lblClassRoster
             // 
             lblClassRoster.AutoSize = true;
             lblClassRoster.Font = new Font("Inter", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-=======
-            // btnExportSummary
-            // 
-            btnExportSummary.BackColor = Color.FromArgb(39, 165, 153);
-            btnExportSummary.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            btnExportSummary.ForeColor = Color.White;
-            btnExportSummary.Location = new Point(727, 107);
-            btnExportSummary.Name = "btnExportSummary";
-            btnExportSummary.Size = new Size(121, 28);
-            btnExportSummary.Style.BackColor = Color.FromArgb(39, 165, 153);
-            btnExportSummary.Style.ForeColor = Color.White;
-            btnExportSummary.TabIndex = 5;
-            btnExportSummary.Text = "Export Summary";
-            btnExportSummary.UseVisualStyleBackColor = false;
-            btnExportSummary.Click += btnExportSummary_Click;
-            // 
-            // btnAddStudent
-            // 
-            btnAddStudent.BackColor = Color.FromArgb(39, 165, 153);
-            btnAddStudent.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            btnAddStudent.ForeColor = Color.White;
-            btnAddStudent.Location = new Point(883, 107);
-            btnAddStudent.Name = "btnAddStudent";
-            btnAddStudent.Size = new Size(113, 28);
-            btnAddStudent.Style.BackColor = Color.FromArgb(39, 165, 153);
-            btnAddStudent.Style.ForeColor = Color.White;
-            btnAddStudent.TabIndex = 6;
-            btnAddStudent.Text = "+ Add Student";
-            btnAddStudent.UseVisualStyleBackColor = false;
-            btnAddStudent.Click += btnAddStudent_Click;
-            // 
-            // lblClassRoster
-            // 
-            lblClassRoster.AutoSize = true;
-            lblClassRoster.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
-            lblClassRoster.ForeColor = Color.FromArgb(29, 37, 48);
->>>>>>> features2/balansag/settings
             lblClassRoster.Location = new Point(109, 474);
             lblClassRoster.Name = "lblClassRoster";
             lblClassRoster.Size = new Size(163, 35);
@@ -210,52 +159,6 @@
             layoutStudents.Size = new Size(887, 195);
             layoutStudents.TabIndex = 10;
             // 
-<<<<<<< HEAD
-=======
-            // lblBackToClass
-            // 
-            lblBackToClass.AutoSize = true;
-            lblBackToClass.Cursor = Cursors.Hand;
-            lblBackToClass.Font = new Font("Segoe UI", 12F);
-            lblBackToClass.ForeColor = Color.FromArgb(29, 37, 48);
-            lblBackToClass.Location = new Point(109, 22);
-            lblBackToClass.Name = "lblBackToClass";
-            lblBackToClass.Size = new Size(115, 21);
-            lblBackToClass.TabIndex = 11;
-            lblBackToClass.Text = "< Back to Class";
-            lblBackToClass.Click += lblBackToClass_Click;
-            // 
-            // btnMarkAllPresent
-            // 
-            btnMarkAllPresent.BackColor = Color.FromArgb(39, 165, 153);
-            btnMarkAllPresent.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            btnMarkAllPresent.ForeColor = Color.White;
-            btnMarkAllPresent.Location = new Point(109, 248);
-            btnMarkAllPresent.Name = "btnMarkAllPresent";
-            btnMarkAllPresent.Size = new Size(121, 28);
-            btnMarkAllPresent.Style.BackColor = Color.FromArgb(39, 165, 153);
-            btnMarkAllPresent.Style.ForeColor = Color.White;
-            btnMarkAllPresent.TabIndex = 12;
-            btnMarkAllPresent.Text = "Mark All Present";
-            btnMarkAllPresent.UseVisualStyleBackColor = false;
-            btnMarkAllPresent.Click += btnMarkAllPresent_Click;
-            // 
-            // btnReset
-            // 
-            btnReset.BackColor = Color.FromArgb(39, 165, 153);
-            btnReset.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
-            btnReset.ForeColor = Color.White;
-            btnReset.Location = new Point(256, 248);
-            btnReset.Name = "btnReset";
-            btnReset.Size = new Size(121, 28);
-            btnReset.Style.BackColor = Color.FromArgb(39, 165, 153);
-            btnReset.Style.ForeColor = Color.White;
-            btnReset.TabIndex = 13;
-            btnReset.Text = "Reset Day";
-            btnReset.UseVisualStyleBackColor = false;
-            btnReset.Click += btnReset_Click;
-            // 
->>>>>>> features2/balansag/settings
             // lblNumberofStudents
             // 
             lblNumberofStudents.AutoSize = true;
@@ -467,23 +370,25 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
             BackColor = Color.FromArgb(249, 250, 251);
-<<<<<<< HEAD
             Controls.Add(kryptonButton8);
             Controls.Add(btnReset);
             Controls.Add(btnMarkAllPresent);
             Controls.Add(btnAddStudent);
             Controls.Add(btnExportSummary);
             Controls.Add(kryptonButton1);
-=======
->>>>>>> features2/balansag/settings
             Controls.Add(pnlExcused);
             Controls.Add(pnlAbsent);
             Controls.Add(pnlLate);
             Controls.Add(pnlPresent);
             Controls.Add(pnlTotalStudents);
             Controls.Add(lblNumberofStudents);
+            Controls.Add(btnReset);
+            Controls.Add(btnMarkAllPresent);
+            Controls.Add(lblBackToClass);
             Controls.Add(layoutStudents);
             Controls.Add(lblClassRoster);
+            Controls.Add(btnAddStudent);
+            Controls.Add(btnExportSummary);
             Controls.Add(lblSubjectName);
             Controls.Add(panelCalendar);
             Controls.Add(lblSectionName);
