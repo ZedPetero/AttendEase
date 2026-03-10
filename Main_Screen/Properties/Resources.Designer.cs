7dd5a6e4d651682fb0be1a0ba8c62510ab452c28
jbalbero553703 <j.balbero.553703@umindanao.edu.ph>
2026-03-10 16:53:12 +0800

    Made a trial new UserLoginForm to test KryptonForm. Added a comment at Program.cs. Added images.

--- Main_Screen/Properties/Resources.Designer.cs
+++ Main_Screen/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace AE.Application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap circle_red {
+            get {
+                object obj = ResourceManager.GetObject("circle-red", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap homepageimg {
             get {
                 object obj = ResourceManager.GetObject("homepageimg", resourceCulture);
